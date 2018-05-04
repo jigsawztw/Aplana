0507cb7558452608d119818302aa1fd2a4802f05
“Ivan <bamby4a364@gmail.com>
2018-05-04 13:31:34 +0300

    во все файлы директории имя которых начинается с v записываем в конец L ;

---
+++ src/WriterToFilesInDir.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+
+public class WriterToFilesInDir {
+        public static void main(String[] args)
+        {
+
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.println("Введите путь к папке: ");
+                File dir = new File(sc.nextLine());
+                if(dir.isDirectory()){
+                    for(File item : dir.listFiles()){
+                        if(item.getName().startsWith("v")){
+
+                            FileWriter writer = new FileWriter(item, true);
+                            BufferedWriter bufferWriter = new BufferedWriter(writer);
+                            bufferWriter.write("L");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+
+        }
+    }
